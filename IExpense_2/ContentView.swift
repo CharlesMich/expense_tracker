34619d971b5aa84ccc411a9cae4707b14c9dcf9c
CharlesMich <charleedongo@hotmail.com>
2025-01-28 11:09:13 -0600

    Initial Commit

---
+++ IExpense_2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IExpense_2
+//
+//  Created by Charles Michael on 1/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IExpense_2/IExpense_2App.swift
@@ -0,0 +1,17 @@
+//
+//  IExpense_2App.swift
+//  IExpense_2
+//
+//  Created by Charles Michael on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct IExpense_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
